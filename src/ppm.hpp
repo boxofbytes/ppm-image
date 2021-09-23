1f9b16989c375fe1f63d4c34f7e8e1d8b102fd02
boxofbytes <77898210+boxofbytes@users.noreply.github.com>
2021-09-23 19:28:15 +0200

    Added image_write() and minor changes.
    Added an `image_write()` function which takes an `image` structure as an argument and writes it to a file correctly. Made changes to the `image` structure and `image_open()` functions.

--- src/ppm.hpp
+++ src/ppm.hpp
@@ -30,7 +30,8 @@ struct image {
    std::vector<rgb> buffer;
    uint64_t width;
    uint64_t height;
-   uint16_t original_index;
+   std::vector<char> key;
+   uint64_t size;
 };
 
 /* function for opening a PPM image, it returns an image class with the */
@@ -42,6 +43,7 @@ image image_open(const std::string& image_path) {
    string_stream << file.rdbuf();
    const std::string& str = string_stream.str();
    std::vector<char> buffer(str.begin(), str.end());
+   uint64_t size = buffer.size();
    std::vector<char> char_vector;
    uint64_t image_width;
    uint64_t image_height;
@@ -87,6 +89,10 @@ image image_open(const std::string& image_path) {
    std::vector<rgb> pixel_buffer;
    rgb current_pixel;
    current_pixel.clear();
+   std::vector<char> key;
+   for(uint64_t i = 0; i < index; i++) {
+      key.push_back(buffer[i]);
+   }
    ++index;
    uint64_t original_index = index;
    for(; index <= buffer.size(); index++) {
@@ -115,7 +121,9 @@ image image_open(const std::string& image_path) {
    returned_image.buffer = pixel_buffer;
    returned_image.width = image_width;
    returned_image.height = image_height;
-   returned_image.original_index = original_index;
+   returned_image.key = key;
+   returned_image.size = size;
+   file.close();
    return returned_image;
 }
 
@@ -131,6 +139,43 @@ void image_invert(image& given_image) {
       new_pixel.b = 255 - current_pixel.b;
       given_image.buffer[i] = new_pixel;
    }
+}
+
+void image_replace(image& given_image, const rgb& old_color, const rgb& new_color) {
+   rgb new_pixel;
+   rgb current_pixel;
+   for(uint64_t i = 0; i < given_image.width * given_image.height; i++) {
+      current_pixel = given_image.buffer[i];
+      new_pixel = current_pixel;
+      if(current_pixel.r == old_color.r && current_pixel.g == old_color.g && current_pixel.b == old_color.b) {
+         new_pixel = new_color;
+      }
+      given_image.buffer[i] = new_pixel;
+   }
+}
+
+void image_write(const image& given_image, const std::string& image_name) {
+   std::ofstream file;
+   file.open(image_name);
+   assert(file.is_open() != false);
+   std::string data;
+   for(uint64_t i = 0; i < given_image.key.size(); i++) {
+      data.push_back(given_image.key[i]);
+   }
+   uint64_t index = given_image.key.size() + 1;
+   for(uint64_t j = 0; j < given_image.width * given_image.height; j++) {
+      for(uint8_t i = 0; i < 3; i++) {
+         if(i == 0) {
+            data.push_back(given_image.buffer[j].r);
+         } else if (i == 1) {
+            data.push_back(given_image.buffer[j].g);
+         } else {
+            data.push_back(given_image.buffer[j].b);
+         }
+      }
+   }
+   file << data;
+   file.close();
 }
 
 #endif
